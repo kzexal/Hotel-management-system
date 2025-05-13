56808f0d133bc4e525fc236c7704763a4a3c6bed
kzexal <buiduykhanh8904@gmail.com>
2025-05-13 21:40:40 +0700

    cập nhật thanh toán

--- WebApplication1/WebApplication1/Controllers/PaymentController.cs
+++ WebApplication1/WebApplication1/Controllers/PaymentController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Text.RegularExpressions;
 using System.Web.Mvc;
 
 namespace WebApplication1.Controllers
@@ -13,23 +14,29 @@ namespace WebApplication1.Controllers
         {
             return View();
         }
+
         public ActionResult PaymentSuccess()
         {
             return View();
         }
+
         [HttpPost]
         public ActionResult ProcessPayment(string CardNumber, string Bank, string ExpDate, string CVV)
         {
-            
-            if (CardNumber == "1234123412341234" && CVV == "123")
-            {
-                return RedirectToAction("PaymentSuccess");
-            }
-            else
+            // Kiểm tra null hoặc rỗng
+            if (string.IsNullOrWhiteSpace(CardNumber) || string.IsNullOrWhiteSpace(CVV))
             {
                 return RedirectToAction("PaymentFailed");
             }
-        }
 
+            // Kiểm tra nếu CardNumber hoặc CVV chứa ký tự không phải số (chữ cái, ký hiệu,...)
+            if (!Regex.IsMatch(CardNumber, @"^\d+$") || !Regex.IsMatch(CVV, @"^\d+$"))
+            {
+                return RedirectToAction("PaymentFailed");
+            }
+
+            // Nếu hợp lệ
+            return RedirectToAction("PaymentSuccess");
+        }
     }
 }
